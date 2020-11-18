e2b5558e5424e18da98bbd78db265fd91d0e993c
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-11-18 19:24:10 +0100

    first test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+constructor (props){
+    super(props);
+    this.state={
+    group:"Gruppe 5"
+
+    }
+}
+  render() {
+    return (
+
+      <div className="App">
+
+        <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
+           <h2 style={{color:"red"}}> This is a test! </h2>
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
