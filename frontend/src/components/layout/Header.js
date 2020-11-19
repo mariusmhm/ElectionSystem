c7fe6c2156d05676df8885bbf44ff48ced8c0028
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-11-19 11:28:07 +0100

    Added folders to frontend

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  render() {
+    return (
+
+      <div className="App">
+
+        <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
+           <h2 style={{color:"red"}}> This will be the Header !</h2>
+
+      </div>
+    );
+  }
+}
+
+export default App;
