adfd197e19734135dc4a366560463ec689293f47
Marius Münstermann <mariusmhm@Mariuss-MBP.fritz.box>
2020-11-20 15:38:03 +0100

    registration react not filled yet

---
+++ frontend/src/components/layout/pages/register.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Grid, Typography, withStyles } from '@material-ui/core';
+
+class Registration extends Component {
+
+}
+
+export default Registration;
