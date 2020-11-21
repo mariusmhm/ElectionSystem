aba12a788d4dde3a413283f812c31208033bcdd0
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-11-21 20:58:59 +0100

    GradingBO completed

---
+++ frontend/src/api/GradingBO.js
@@ -0,0 +1,46 @@
+import BusinessObject from './BusinessObject'; // Don`t know if this is needed
+import NamedBusinessObject from './NamedBusinessObject';
+
+
+export default class UserBO extends NamedBusinessObject {
+
+/// Represents a grade which can be given a student.
+
+    constructor(aGrading){
+        super();
+        this.grading=aGrading;
+
+    //Sets the grading
+    setGrading(aGrading){
+      this.grading = aGrading;
+    }
+
+    //Reads out the Grading
+    getGrading(){
+        return this.grading;
+    }
+
+    //Returns an Array of CustomerBOs from a given JSON structure.
+
+    static fromJSON(gradings) {
+        let res = [];
+
+        if (Array.isArray(gradings)) {
+            gradings.forEach((s) => {
+                Object.setPrototypeOf(s, GradingBO.prototype);
+                res.push(s);
+            })
+        }
+        // it's a single object and not an array
+        else {
+            let s = users;
+            Object.setPrototypeOf(s, GradingBO.prototype);
+            res.push(s);
+        }
+
+        return res;
+
+
+}
+)
+}
---
+++ frontend/src/api/UserBO.js
@@ -0,0 +1,81 @@
+import BusinessObject from './BusinessObject'; // Don`t know if this is needed
+import NamedBusinessObject from './NamedBusinessObject';
+
+
+export default class UserBO extends NamedBusinessObject {
+
+/// Represents a user of the electionsystem. It is either a sutdent or a professor.
+
+
+    constructor(aUserId, aEmail, aPassword, aRole){
+        super();
+        this.userId=aUserId;
+        this.email=aEmail;
+        this.password=aPassword;
+        this.role=aRole;
+
+   //Sets the UserId
+    setUserId(aUserId){
+        this.userId = aUserId;
+    }
+    //Reads out the UserId
+    getUserId(){
+        return this.userId;
+    }
+
+    //Sets the Users Email
+    setUserMail(aEmail){
+        this.email = aEmail;
+
+    }
+
+    //Reads out Users Email
+    getUserMail(){
+        return this.email
+
+    }
+
+    //Sets the Users password
+    setPassword(paassword){
+        this.password = aPassword
+
+    }
+
+    //Reads out the Users Password
+    getPassword(){
+        return this.password
+
+    }
+
+    //sets the Users Role
+    setRole(aRole){
+        this.role = aRole
+    }
+
+    //Returns an Array of CustomerBOs from a given JSON structure.
+
+    static fromJSON(students) {
+        let res = [];
+
+        if (Array.isArray(users)) {
+            users.forEach((s) => {
+                Object.setPrototypeOf(s, UserBO.prototype);
+                res.push(s);
+            })
+        }
+        // it's a single object and not an array
+        else {
+            let s = users;
+            Object.setPrototypeOf(s, UserBO.prototype);
+            res.push(s);
+        }
+
+        return res;
+
+
+
+
+
+}
+)
+}
