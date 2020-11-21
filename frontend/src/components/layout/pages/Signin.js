6d71c5a8ddb96106265f1b19d6d618028d1c5183
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-11-21 13:36:49 +0100

    small changes in frontend

---
+++ frontend/src/components/layout/pages/Signin.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+class Signin extends Component {
+constructor (props){
+    super(props);
+    this.state={
+    group:"Gruppe 5"
+
+    }
+}
+  render() {
+    return (
+
+      <div className="App">
+
+        <Grid container, direction="row", spacing={2} justify="space-evenly", alignItems="stretch">
+
+
+
+
+        </Grid>
+        <Grid container, direction="row", justify="space-evenly", alignItems="stretch">
+
+
+
+        </Grid>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function AutoGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+
