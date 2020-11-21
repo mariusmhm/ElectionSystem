1ae715dd11e6e754760127fb2a00c031fc85deaf
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-11-21 17:32:49 +0100

    Completed the basic structure of signin.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Grid from '@material-ui/core/Grid';
+import { Button, Icon, Container, TextField, Typography, withStyles } from '@material-ui/core';
 
 //This is for testing approaches
 
@@ -12,11 +15,71 @@ constructor (props){
 }
   render() {
     return (
+    //This is for testing, do not delete:
 
       <div>
-        <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
-        <h2 style={{color:"red", textAlign:"center"}} > This is a test! </h2>
-     </div>
+      <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
+      <h2 style={{color:"red", textAlign:"center"}} > This is a test! </h2>
+
+        <Container maxWidth="sm">
+            <Grid item xs={12} sm={6}>
+                <Grid container spacing={1} style={{textAlign:"center", backgroundColor:"white"}}>
+                <Typography variant="h3" style={{textAlign:"center"}}>Wilkommen</Typography>
+                    < Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <TextField
+                             id="outlined-basic"
+                             label="E-Mail"
+                             variant="outlined"
+                             label="E-Mail"
+                            />
+                        </form>
+                    </Grid>
+                    < Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <TextField
+                               id="outlined-basic"
+                               label="Passwort"
+                               variant="outlined"
+                               label="Passwort"
+                               type="password"
+                            />
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained" color="secondary">
+                                Log In
+                            </Button>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+
+        <Container maxWidth="sm">
+            <Grid item xs={12} sm={6}>
+                <Grid container spacing={1} style={{textAlign:"center", backgroundColor:"gray"}}>
+                <Typography variant="h3" style={{textAlign:"center", backgroundColor:"gray"}}>Neu Hier?</Typography>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained">
+                                Als Student registrieren
+                            </Button>
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained">
+                                Als Dozent registrieren
+                            </Button>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Gird>
+
+        </Container>
+      </div>
 
     );
   }
--- frontend/src/components/layout/pages/Signin.js
+++ frontend/src/components/layout/pages/Signin.js
@@ -1,65 +1,84 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Grid from '@material-ui/core/Grid';
+import { Button, Icon, Container, TextField, Typography, withStyles } from '@material-ui/core';
 
+//This will be the Signin page for students and Professors
 
-// Registration only for Testing right now
+class Signin extends Component {
 
-class App extends Component {
-constructor (props){
-    super(props);
-    this.state={
-    group:"Gruppe 5"
-
-    }
 }
   render() {
     return (
+    //This is for testing, do not delete:
+    //It is not finished yet
 
       <div>
-        <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
-        <h2 style={{color:"red", textAlign:"center"}} > This is a test! </h2>
+      <h1 style= {{color:"red", textAlign:"center"}}> Hello World!</h1>
+      <h2 style={{color:"red", textAlign:"center"}} > This is a test! </h2>
 
-      <Container maxWidth="sm">
-        <Grid container spacing={1} style={{backgroundColor:"gray"}}>
-            <Paper >Willkommen </Paper>
-                <form noValidate autoComplete="off" style={{}}>
-                    <TextField
-                    id="outlined-basic"
-                    label="E-Mail"
-                    variant="outlined"
-                    label="E-Mail"
-                    />
+        <Container maxWidth="sm">
+            <Grid item xs={12} sm={6}>
+                <Grid container spacing={1} style={{textAlign:"center", backgroundColor:"white"}}>
+                <Typography variant="h3" style={{textAlign:"center"}}>Wilkommen</Typography>
+                    < Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <TextField
+                             id="outlined-basic"
+                             label="E-Mail"
+                             variant="outlined"
+                             label="E-Mail"
+                            />
+                        </form>
+                    </Grid>
+                    < Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <TextField
+                               id="outlined-basic"
+                               label="Passwort"
+                               variant="outlined"
+                               label="Passwort"
+                               type="password"
+                            />
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained" color="secondary">
+                                Log In
+                            </Button>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
 
-                    <TextField
-                    id="outlined-basic"
-                    label="Passwort"
-                    variant="outlined"
-                    label="Passwort"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="filled"
-                    />
+        <Container maxWidth="sm">
+            <Grid item xs={12} sm={6}>
+                <Grid container spacing={1} style={{textAlign:"center", backgroundColor:"gray"}}>
+                <Typography variant="h3" style={{textAlign:"center", backgroundColor:"gray"}}>Neu Hier?</Typography>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained">
+                                Als Student registrieren
+                            </Button>
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <form noValidate autoComplete="off" style={{}}>
+                            <Button variant="contained">
+                                Als Dozent registrieren
+                            </Button>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Gird>
 
-                    <Button variant="contained" color="secondary">
-                    Log In
-                    </Button>
-
-                </form>
-
-
-        </Grid>
-
-     </Container>
-
-     </div>
+        </Container>
+      </div>
 
     );
   }
 }
 
 export default App;
-
-
-
-
