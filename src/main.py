1e3c3a6b3e425d7426a82fb04e793281fd036ecb
SarandaGojani <73344482+SarandaGojani@users.noreply.github.com>
2020-11-22 16:47:12 +0100

    Aufbau Main

--- src/main.py
+++ src/main.py
@@ -23,6 +23,88 @@ app = Flask(__name__)
 
 
 
+CORS(app, resources=r'/Electionsystem/*')
 
+
+
+"""Hier wird eine API angelegt, 
+auf deren Basis Clients und Server Daten austauschen. Grundlage hierfür ist das Package flask-restx."""
+
+api = Api(app, version='1.0', title='Electionsystem API',
+          description='Ein Wahlsystem für Studenten')
+
+
+
+"""Namespaces erlauben uns die Strukturierung von APIs. In diesem Fall fasst dieser Namespace alle
+ElectionSystem-relevanten Operationen unter dem Präfix /bank zusammen."""
+
+ElectionSystem = api.namespace('Electionsystem', description='Funktionen des Electionsystems')
+
+
+"""Nachfolgend werden analog zu unseren BusinessObject-Klassen und NamedBusinessObject-Klassen
+ die transferierbare Strukturen angelegt:
+
+BusinessObject dient als Basisklasse, auf der die weiteren Strukturen Teilnahme und Bewertung aufsetzen.
+ ab und """
+
+bo = api.model('BusinessObject', {
+    'id': fields.Integer(attribute='__id', description='Der Unique Identifier eines Business Object'),
+})
+
+"""NamedBusinessObject leitet von Business Object ab"""
+nbo = api.model('NamedBusinessObject',bo, {
+    'name': fields.Integer(attribute='__name', description='Der Name eines NamedBusiness Object'),
+})
+
+"""NamedBusinessObject setzt weiter Strukturen auf, wie User, Student, Grading, Module,
+Participation, Project, Projecttype und Semester."""
+
+user = api.inherit('User', nbo, {
+    'user_id': fields.String(attribute='__user_id', description='ID eines User'),
+    'email': fields.String(attribute='__email', description='E-Mail-Adresse eines User'),
+    'role': fields.String(attribute='__role', description='Role eines User'),
+    'password': fields.String(attribute='__password ', description='Password eines User'),
+})
+
+student = api.inherit('Student', nbo, {
+    'student_id': fields.String(attribute='__student_id', description='Id eines Studenten'),
+    'student_name': fields.String(attribute='__student_name', description='Name eines Studenten'),
+    'mail': fields.String(attribute='__mail', description='E-Mail-Adresse eines Studenten'),
+    'role': fields.String(attribute='__role', description='Google User ID eines User'),
+    'password': fields.String(attribute='__password ', description='Password eines Studenten'),
+    'MatrikelNR': fields.String(attribute='__MatrikelNR', description='MatrikelNR eines Studenten'),
+    'study':fields.String(attribute='__study', description='Studiengang eines Studenten'),
+})
+
+#transferierbare Strukturen die noch eingefügt werden müssen
+#grading=
+
+#module=
+
+#participation=
+
+#project=
+
+#projecttype=
+
+#semester=
+
+#bewertung=
+
+#teilnahme=
+
+
+
+#@ElectionSystem.route('/login', methods=['POST','GET'])
+#@ElectionSystem.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+#def login():
+    #error=None
+    #if request.method=='POST':
+       # if valid_login(request.form['E-Mail'],
+                      # request.form['Passwort']): # MapperKlasse?
+            #return log_the_user_in(request.form['E-Mail'])
+       # else:
+            #error='Invalid E-Mail/Passwort'
+    #return render_template('login.js',error=error)
 
 
