c0cb1b7511f0735a903a27415d058ee671e874d4
Jana Beer <49690605+Jana-Beer@users.noreply.github.com>
2020-12-02 12:51:14 +0100

    added add, get, delete, update Methods

--- frontend/src/api/ElectionSystemAPI.js
+++ frontend/src/api/ElectionSystemAPI.js
@@ -1,6 +1,6 @@
 import GradingBO from './GradingBO';
 import ModuleBO from './ModuleBO';
-import PartcipationBO from './ParticipationBO';
+import ParticipationBO from './ParticipationBO';
 import ProjectBO from './ProjectBO';
 import ProjecttypeBO from './ProjecttypeBO';
 import SemesterBO from './SemesterBO';
@@ -14,27 +14,45 @@ export default class ElectionSystemAPI {
     #electionSystemServerBaseURL ='/electionsystem'
 
     //Project
-    #getProjectsURL = () => `${this.#electionSystemServerBaseURL}/projects`;
+    #getAllProjectsURL = () => `${this.#electionSystemServerBaseURL}/projects`;
     #addProjectURL = () => `${this.#electionSystemServerBaseURL}/projects`;
     #getProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}`;
     #updateProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}`;
     #deleteProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}`;
     
-
-
     //Project type
     #getAllProjecttypesURL = () => `${this.#electionSystemServerBaseURL}/projecttypes`;
     #getProjecttypeForProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}/projecttype`;
     #addProjecttypeURL = () => `${this.#electionSystemServerBaseURL}/projecttype`;
-    #updateProjecttypeURL = (id) => `${this.#electionSystemServerBaseURL}/projecttype/${id}`
+    #updateProjecttypeURL = (id) => `${this.#electionSystemServerBaseURL}/projecttype/${id}`;
     #deleteProjecttypeURL = (id) => `${this.#electionSystemServerBaseURL}/projecttype/${id}`;
 
     //Module
-    #getAllModulesURL = (id) => `${this.#electionSystemServerBaseURL}/modules`;
+    #getAllModulesURL = () => `${this.#electionSystemServerBaseURL}/modules`;
     #getModuleForProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}/module`;
     #addModuleURL = () => `${this.#electionSystemServerBaseURL}/module`;
-    #updateModuleURL = (id) => `${this.#electionSystemServerBaseURL}/module/${id}`
+    #updateModuleURL = (id) => `${this.#electionSystemServerBaseURL}/module/${id}`;
     #deleteModuleURL = (id) => `${this.#electionSystemServerBaseURL}/module/${id}`;
+    
+    //Grading
+    #getAllGradesURL = () => `${this.#electionSystemServerBaseURL}/grades`;
+    #addGradeURL = () => `${this.#electionSystemServerBaseURL}/grade`;
+    #updateGradeURL = (id) => `${this.#electionSystemServerBaseURL}/grade/${id}`;
+    #deleteGradeURL = (id) => `${this.#electionSystemServerBaseURL}/grade/${id}`;
+    
+    //Participation
+    #getAllParticipationsForProjectURL = (id) => `${this.#electionSystemServerBaseURL}/projects/${id}/participations`;
+    #addParticipationURL = () => `${this.#electionSystemServerBaseURL}/participation`;
+    #updateParticipationURL = (id) => `${this.#electionSystemServerBaseURL}/participation/${id}`;
+    #deleteParticipationURL = (id) => `${this.#electionSystemServerBaseURL}/participation/${id}`;
+
+    //Semester
+    #addSemesterURL = () => `${this.#electionSystemServerBaseURL}/semester`;
+    #updateSemesterURL = (id) => `${this.#electionSystemServerBaseURL}/semester/${id}`;
+
+    //Student 
+
+    //User
 
     static getAPI() {
         if (this.#api == null) {
@@ -53,22 +71,299 @@ export default class ElectionSystemAPI {
     }
     )
 
-    getAllModules(){
-        return this.#fetchAdvanced(this.#getAllModulesURL()).then((responseJSON) => {
-            let moduleBOs = ModuleBO.fromJSON(responseJSON);
+    /** 
+    *@public
+    */
+    getAllProjects(){
+        return this.#fetchAdvanced(this.#getAllProjectsURL()).then((responseJSON) => {
+            let projectBOs = ProjectBO.fromJSON(responseJSON);
             return new Promise(function (resolve){
-                resolve(moduleBOs)
+                resolve(projectBOs)
             })
-        }) 
+        })
+    }
+    
+    /** 
+    *@param {ProjectBO} projectBO
+    *@public
+    */
+    addProject(projectBO){
+        return this.#fetchAdvanced(this.#addProjectURL(), {
+            method: 'POST'
+            }).then((responseJSON) => {
+            let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjectBO);
+            })
+          })
     }
 
-    getAllProjecttypes(){
-        return this.#fetchAdvanced(this.#getAllProjecttypesURL())
-        .then((responseJSON)=> {
+    /** 
+    *@param {Number} projectID
+    *@public
+    */
+    getProject(projectID){
+        return this.#fetchAdvanced(this.#getProjectURL(projectID)).then((responseJSON) => {
+            let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjectBO);
+            })
+          })
+    }
+
+    /** 
+    *@param {ProjectBO} projectBO
+    *@public
+    */
+    updateProject(){
+        return this.#fetchAdvanced(this.#updateProjectURL(projectBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjectBO);
+            })
+          })
+    }
+
+    /** 
+    *@param {Number} projectID
+    *@public
+    */
+    deleteProject(projectID){
+        return this.#fetchAdvanced(this.#deleteProjectURL(projectID), {
+            method: 'DELETE'
+          }).then((responseJSON) => {
+            let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjectBO);
+            })
+          })
+    }
+
+     /** 
+    *@public
+    */
+   getAllProjecttypes(){
+        return this.#fetchAdvanced(this.#getAllProjecttypesURL()).then((responseJSON)=> {
             let projecttypeBOs =ProjecttypeBO.fromJSON(responseJSON);
+            console.info(projecttypeBOs);
             return new Promise(function(resolve){
                 resolve(projecttypeBOs);
             })
         })
     }
+
+    getProjecttypeForProject(projectID){
+        return this.#fetchAdvanced(this.#getProjecttypeForProjectURL(projectID))
+        .then((responseJSON) => {
+        let projecttypeBOs = ProjecttypeBO.fromJSON(responseJSON);
+        return new Promise(function (resolve) {
+          resolve(projecttypeBOs);
+        })
+      })
+    }
+
+    addProjecttypeURL(){
+        return this.#fetchAdvanced(this.#addProjecttypeURL(), {
+            method: 'POST'
+          })
+            .then((responseJSON) => {
+              let projecttypeBO = ProjecttypeBO.fromJSON(responseJSON)[0];
+              return new Promise(function (resolve) {
+                resolve(projecttypeBO);
+              })
+            })
+    }
+
+    updateProjecttype(projecttypeBO){
+        return this.#fetchAdvanced(this.#updateProjecttypeURL(projecttypeBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseProjecttypeBO = ProjecttypeBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjecttypeBO);
+            })
+          })
+    }
+
+    deleteProjecttype(projecttypeID){
+        return this.#fetchAdvanced(this.#deleteProjecttypeURL(projecttypeID), {
+            method: 'DELETE'
+          }).then((responseJSON) => {
+            let responseProjecttypeBO = ProjecttypeBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseProjecttypeBO);
+            })
+          })
+    }
+
+    /** 
+    *@public
+    */
+    getAllModules(){
+        return this.#fetchAdvanced(this.#getAllModulesURL()).then((responseJSON) => {
+            let responseModuleBOs = ModuleBO.fromJSON(responseJSON);
+            return new Promise(function (resolve){
+                resolve(responseModuleBOs)
+            })
+        }) 
+    }
+
+    getModuleForProject(projectID){
+        return this.#fetchAdvanced(this.#getModuleForProjectURL(projectID))
+        .then((responseJSON) => {
+        let responseModuleBOs = ModuleBO.fromJSON(responseJSON);
+        return new Promise(function (resolve) {
+          resolve(responseModuleBOs);
+        })
+      })
+    }
+
+    addModule(){
+        return this.#fetchAdvanced(this.#addModuleURL(), {
+            method: 'POST'
+          })
+            .then((responseJSON) => {
+              let responseModuleBO = ModuleBO.fromJSON(responseJSON)[0];
+              return new Promise(function (resolve) {
+                resolve(responseModuleBO);
+              })
+            })
+    }
+
+    updateModule(moduleBO){
+        return this.#fetchAdvanced(this.#updateModuleURL(moduleBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseModuleBO = ModuleBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseModuleBO);
+            })
+          })
+    }
+
+    deleteModule(moduleID){
+        return this.#fetchAdvanced(this.#deleteModuleURL(moduleID), {
+            method: 'DELETE'
+          }).then((responseJSON) => {
+            let responseModuleBO = ModuleBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseModuleBO);
+            })
+          })
+    }
+
+
+    getAllGrades(){
+        return this.#fetchAdvanced(this.#getAllGradesURL()).then((responseJSON) => {
+            let responseGradeBOs = GradingBO.fromJSON(responseJSON);
+            return new Promise(function (resolve){
+                resolve(responseGradeBOs)
+            })
+        }) 
+    }
+    
+    addGrade(){
+        return this.#fetchAdvanced(this.#addGradeURL(), {
+            method: 'POST'
+          })
+            .then((responseJSON) => {
+              let responseGradeBO = GradingBO.fromJSON(responseJSON)[0];
+              return new Promise(function (resolve) {
+                resolve(responseGradeBO);
+              })
+            })
+    }
+
+    updateGrade(gradingBO){
+        return this.#fetchAdvanced(this.#updateGradeURL(gradingBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseGradingBO = GradingBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseGradingBO);
+            })
+          })
+    }
+
+    deleteGrade(gradeID){
+        return this.#fetchAdvanced(this.#deleteGradeURL(gradeID), {
+            method: 'DELETE'
+          }).then((responseJSON) => {
+            let responseGradingBO = GradingBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseGradingBO);
+            })
+          })
+    }
+
+    getAllParticipationsForProject(projectID){
+        return this.#fetchAdvanced(this.#getAllParticipationsForProjectURL(projectID))
+        .then((responseJSON) => {
+        let responseParticipationBOs = ParticipationBO.fromJSON(responseJSON);
+        return new Promise(function (resolve) {
+          resolve(responseParticipationBOs);
+        })
+      })
+    }
+
+    addParticipation(){
+        return this.#fetchAdvanced(this.#addParticipationURL(), {
+            method: 'POST'
+          })
+            .then((responseJSON) => {
+              let responseParticipationBO = ParticipationBO.fromJSON(responseJSON)[0];
+              return new Promise(function (resolve) {
+                resolve(responseParticipationBO);
+              })
+            })
+    }
+
+    updateParticipation(participationBO){
+        return this.#fetchAdvanced(this.#updateParticipationURL(participationBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseParticipationBO = ParticipationBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseParticipationBO);
+            })
+          })
+    }
+
+    deleteParticipation(participationID){
+        return this.#fetchAdvanced(this.#deletePartURL(participationID), {
+            method: 'DELETE'
+          }).then((responseJSON) => {
+            let responseParticipationBO = ParticipationBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseParticipationBO);
+            })
+          })
+    }
+
+    addSemester(){
+        return this.#fetchAdvanced(this.#addSemesterURL(), {
+            method: 'POST'
+          })
+            .then((responseJSON) => {
+              let responseSemesterBO = SemesterBO.fromJSON(responseJSON)[0];
+              return new Promise(function (resolve) {
+                resolve(responseSemesterBO);
+              })
+            })
+    }
+
+    updateSemester(semesterBO){
+        return this.#fetchAdvanced(this.#updateSemesterURL(semesterBO.getID()), {
+            method: 'PUT'
+            }).then((responseJSON) => {
+            let responseSemesterBO = SemesterBO.fromJSON(responseJSON)[0];
+            return new Promise(function (resolve) {
+              resolve(responseSemesterBO);
+            })
+          })
+    }
+
+   
 }
