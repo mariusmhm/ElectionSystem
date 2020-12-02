29f7c841e91422798feffe95217ca790dabf8c8e
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-12-02 21:24:54 +0100

    updated SemesterMapper.py with methods

--- src/server/db/SemesterMapper.py
+++ src/server/db/SemesterMapper.py
@@ -66,7 +66,7 @@ class SemesterMapper(Mapper):
 
         return result
 
-    def find_by_grading_end_date(self,grading_end_date):
+    def find_by_grading_end_date(self, grading_end_date):
         """Read out the semester based on their grading end date.
                 : param grading_end_Date of the associated semester.
                 : return a semester object with the grading end date."""
@@ -98,15 +98,83 @@ class SemesterMapper(Mapper):
 
         return result
 
-    def find_by_submit_projects_end_date(self,submit_projects_end_date):
+    def find_by_submit_projects_end_date(self, submit_projects_end_date):
         """Read out all semesters based on their submit_projects_end_date.
                 : param submit_projects_end_date of the semester.
-                : return A collection of user objects that are all semester
+                : return A collection of semester objects that are all 
                     with the desired submit_projects_end_date."""
 
 
 
-#to be continued 
+    def find_by_wintersemester(self, wintersemester):
+        
+         """Read out semester based on if they are a wintersemester.
+                : param (wintersemester)  wintersemester of the associated semester.
+                : return the wintersemester object that contain
+                    the desired  wintersemester."""
+         
+    def insert(self, semester):
+        """Insertion of a semester object into the database.
+
+                The primary key of the transferred object is also checked and if necessary
+                corrected.
+
+                : param semester the object to be saved
+                : return the object that has already been transferred, but with a possibly corrected ID.
+                """
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM semester ")
+        tuples = cursor.fetchall()
+        
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                """
+                If we determine a central ID we use this
+                by 1 and assign this value as the ID to the semester object. """
+                semester.set_id(maxid[0] + 1)
+            else:
+                """If we CAN'T find a maximum ID, let's
+                assume that the table is empty and that we can start with ID 1. """
+                semester.set_id(1)
+                
+            command = "INSERT INTO semester (id, creation_date, wintersemester, submit_projects_end_date, email, role VALUES (%s,%s,%s,%s,%s,%s)"
+            data = (semester.get_id(), semester.creation_date(), semester.get_submit_projects_end_date(),semester.get_grading_end_date(),semester.get_wintersemester(),)
+            cursor.execute(command, data)
+
+        self._connection.commit()
+        cursor.close()
+
+        return semester
+    
+    def update(self, semester):
+        
+        """Repeated writing of an object to the database.
+        : param semester the object that is to be written to the DB"""
+        
+        cursor = self._connection.cursor()
+
+        command = "UPDATE semester " + "SET wintersemester=%s, creation_date=%s, submit_projects_end_date=%s, grading_end_date=%s  WHERE semester_id=%s"
+        data = (semester.get_wintersemester(), semester.get_grading_end_date(), semester.get_submit_projects_end_date())
+        cursor.execute(command, data)
+
+        self._connection.commit()
+        cursor.close()
+        
+        
+    def delete(self, semester):
+        """Deleting the data of a semester object from the database.
+
+        : param semester the "object" to be deleted from the DB
+        """
+        cursor = self._connection.cursor()
+
+        command = "DELETE FROM semester WHERE id={}".format(semester.get_id())
+        cursor.execute(command)
+
+        self._connection.commit()
+        cursor.close()
+
+
 
 
 
