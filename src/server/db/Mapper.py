84e1beff5e4c10ab56251fa5f64d6d57fc1303eb
Adrian Awad <adrianawad@Adrians-iMac.fritz.box>
2020-12-04 21:21:26 +0100

    Database Connection

--- src/server/db/Mapper.py
+++ src/server/db/Mapper.py
@@ -10,7 +10,7 @@ class Mapper (AbstractContextManager, ABC):
         self._connection = None
 
     def __enter__(self):
-        self._connection = connector.connect(user="root", password="12345678", host="127.0.0.1", database="electionsystem")
+        self._connection = connector.connect(user='web357_35', password='XfJbuWNoVCpdnx5l', host='s217.goserver.host', database='web357_db35')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
