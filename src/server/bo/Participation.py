ad8c79aefb4d79f832abf8ef269586b8d3af9cf5
SarandaGojani <73344482+SarandaGojani@users.noreply.github.com>
2020-12-04 21:45:58 +0100

    Update Participation.py

--- src/server/bo/Participation.py
+++ src/server/bo/Participation.py
@@ -1,67 +1,35 @@
 from server.bo.BusinessObject import BusinessObject
-from server.bo.NamedBusinessObject import NamedBusinessObject
 
-class Participation(NamedBusinessObject):
 
+class Participation(BusinessObject):
 
     def __init__(self):
-
-        self.__student = Student()
-        self.__project = Project()
-        self.__grading  = Grading()
-        self.__priority = []
-
-    def get_student(self):
-        """Reads out the student."""
-        return self.__student
-
-    def set_student(self, student):
-        """Sets the student."""
-        self.__student = student
-
-    def get_project(self):
-        """Reads out the project."""
-        return self.__project
-
-    def set_project(self, project):
-        """Sets the project."""
-        self.__project = project
-
-    def get_grading(self):
-        """Reads out the grading"""
-        return self.__grading
-
-    def set_grading(self, grading):
-        """Sets the grading"""
-        self.__grading = grading
+        super().__init__()
+        self._priority =[]
 
     def get_priority(self):
         """Reads out the priority."""
-        return self.__priority
+        return self._priority
 
     def set_priority(self, priority):
         """Sets the priority."""
-        self.__priority = priority
+        self._priority = priority
 
     def __str__(self):
 
-        """Creats a simple textually Representation of a Participation() instanz.
-        """
+        """Creats a simple textually Representation of a Participation() instanz."""
+  
 
-        return "Project:  {}, {}, {}, {} ".format(self.get_grading(),self.get_priority(),self.get_project(), self.get_student())
+        return "Project:  {}, {}, ".format(self.get_id(),self.get_priority(),)
 
 
     @staticmethod
     def to_dict(dicti=dict()):
-
         """"Convert  a Python dict() in a Participation()."""
         participation = Participation()
-        participation.set_grading(dicti["ParticipationGrading"])
-        participation.set_priority(dicti["ParticipationPriority"])
-        participation.set_project(dicti["ParticipationProject"])
-        participation.set_student(dicti["ParticipationStudent"])
+        participation.set_id(dicti["id"])
+        participation.set_priority(dicti["participation_priority"])
         return participation
-
 
 
 
