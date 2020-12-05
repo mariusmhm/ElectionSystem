d156b0844c49cc31f1dc85232fb0a0dc304807af
Marius Münstermann <mm284@hdm-stuttgart.de>
2020-12-05 19:16:32 +0100

    finished participationmapper

--- src/server/db/ParticipationMapper.py
+++ src/server/db/ParticipationMapper.py
@@ -14,13 +14,10 @@ class ParticipationMapper (Mapper):
         cursor.execute("SELECT * from participation")
         tuples = cursor.fetchall()
 
-        for (participation_id, creation_date, student_id, project_id, grading_id, priority) in tuples:
+        for (participation_id, creation_date, priority) in tuples:
             participation = Participation()
             participation.set_id(participation_id)
             participation.set_creation_date(creation_date)
-            participation.set_student(student_id)
-            participation.set_project(project_id)
-            participation.set_grading(grading_id)
             participation.set_priority(priority)
             result.append(participation)
 
@@ -43,13 +40,10 @@ class ParticipationMapper (Mapper):
         tuples = cursor.fetchall()
 
         try:
-            (participation_id, creation_date, student, project, grading, priority) = tuples[0]
+            (participation_id, creation_date, priority) = tuples[0]
             participation = Participation()
             participation.set_id(participation_id)
             participation.set_creation_date(creation_date)
-            participation.set_student(student)
-            participation.set_project(project)
-            participation.set_grading(grading)
             participation.set_priority(priority)
             result = participation
 
@@ -61,74 +55,6 @@ class ParticipationMapper (Mapper):
         self._connection.commit()
         cursor.close()
         return result
-
-    def find_by_student_id(self, student_id):
-        """Read out the participations based on the student.
-        : param student of the associated participations.
-        : return a participation object with the student number."""
-
-        result = None
-        cursor = self._connection.cursor()
-        command = "SELECT participation_id, creation_date, student_id, project_id, grading_id, priority FROM participations WHERE student_id={}" \
-                  .format(student_id)
-
-        cursor.execute(command)
-        tuples = cursor.fetchall()
-
-        try:
-            (participation_id, creation_date, student_id, project_id, grading_id, priority) = tuples[0]
-            participation = Participation()
-            participation.set_id(participation_id)
-            participation.set_creation_date(creation_date)
-            participation.set_student(student_id)
-            participation.set_project(project_id)
-            participation.set_grading(grading_id)
-            participation.set_priority(priority)
-            result = participation
-
-        except IndexError:
-            """The IndexError will occur above when accessing tuples [0] when the previous SELECT call
-                       does not return tuples, but tuples = cursor.fetchall () returns an empty sequence."""
-            result = None
-
-        self._connection.commit()
-        cursor.close()
-        return result
-
-        
-    def find_by_project_id(self, project):
-        """Read out the participations based on the project.
-        : param project of the associated participations.
-        : return a participation object with the project number."""
-
-        result = None
-        cursor = self._connection.cursor()
-        command = "SELECT participation_id, creation_date, student_id, project_id, grading_id, priority FROM participations WHERE project_id={}" \
-                  .format(project_id)
-
-        cursor.execute(command)
-        tuples = cursor.fetchall()
-
-        try:
-            (participation_id, creation_date, student_id, project_id, grading_id, priority) = tuples[0]
-            participation = Participation()
-            participation.set_id(participation_id)
-            participation.set_creation_date(creation_date)
-            participation.set_student(student_id)
-            participation.set_project(project_id)
-            participation.set_grading(grading_id)
-            participation.set_priority(priority)
-            result = participation
-
-        except IndexError:
-            """The IndexError will occur above when accessing tuples [0] when the previous SELECT call
-                       does not return tuples, but tuples = cursor.fetchall () returns an empty sequence."""
-            result = None
-
-        self._connection.commit()
-        cursor.close()
-        return result
-
         
     def find_by_priority(self, priority):
         """Read out the participation based on their priority.
@@ -144,13 +70,10 @@ class ParticipationMapper (Mapper):
         tuples = cursor.fetchall()
 
         try:
-            (participation_id, creation_date, student_id, project_id, grading_id, priority) = tuples[0]
+            (participation_id, creation_date, priority) = tuples[0]
             participation = Participation()
             participation.set_id(participation_id)
             participation.set_creation_date(creation_date)
-            participation.set_student(student_id)
-            participation.set_project(project_id)
-            participation.set_grading(grading_id)
             participation.set_priority(priority)
             result = participation
 
@@ -162,3 +85,53 @@ class ParticipationMapper (Mapper):
         self._connection.commit()
         cursor.close()
         return result
+
+    def insert(self, participation):
+        """Insertion of a participation object into the database.
+
+        The primary key of the transferred object is also checked and if necessary
+        corrected.
+
+        : param participation the object to be saved
+        : return the object that has already been transferred, but with a possibly corrected ID.
+        """
+        crs = self._connection.cursor()
+        crs.execute("SELECT MAX(id) AS max_id FROM Participation")
+        tuples = crs.fetchall()
+
+        for (max_id) in tuples:
+            participation.set_id(max_id[0]+1)
+
+            crs.execute("INSERT INTO Participation(participation_id, creation_date, priority) VALUES ('{}', '{}','{}')".format(participation.get_id(), participation.get_creation_date(), participation.get_priority()))
+        
+        self._connection.commit()
+        crs.close()
+
+        return participation
+
+
+    def delete(self, participation):
+
+        """Deleting the data of a participation object from the database.
+        : param participation the "object" to be deleted from the DB"""
+
+        crs = self._connection.cursor()
+        crs.execute("DELETE FROM Participation WHERE participation_id={}".format(participation.get_id()))
+
+        self._connection.commit()
+        crs.close()
+
+
+    def update(self, participation):
+
+        """Repeated writing of an object to the database.
+            : param participation the object to be written into the DB"""
+        cursor = self._connection.cursor()
+
+        command = "UPDATE Participation " + "SET creation_date=%s, priority=%s WHERE participation_id=%s"
+        data = (participation.get_creation_date(), participation.get_priority(), participation.get_id())
+        cursor.execute(command, data)
+
+        self._connection.commit()
+        cursor.close()
+        
