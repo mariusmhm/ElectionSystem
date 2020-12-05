8e83174db7c6b3605099d53a889d226229aaf718
Kevin Bahnmueller <kb130@hdm-stuttgart.de>
2020-12-05 13:26:42 +0100

    Project.py

--- src/server/bo/Project.py
+++ src/server/bo/Project.py
@@ -1,34 +1,45 @@
-from server.bo.BusinessObject import BusinessObject
-from server.bo.NamedBusinessObject import NamedBusinessObject
+from server.bo import BusinessObject as bo
 
 
 
 
-
-class Project(NamedBusinessObject):
+class Project(bo.BusinessObject):
 
     """Realization of an examplary Project.
     """
 
     def __init__(self):
         super().__init__()
-        self.__project_id = []
-        self.__project_name = None
-        self.__project_description = None
+        self._project_id = None
+        self._project_name = None
+        self._project_description = None
 
-    def __str__(self):
+    def set_project_id(self, id):
+        return self._project_id == id
+    
+    def get_project_id(self):
+        return self._project_id
 
-        """Creats a simple textually Representation of a Project() instanz.
-        """
+    def set_project_name(self, name):
+        return self._project_name == name 
+    
+    def get_project_name(self):
+        return self._project_name
 
-        return "Project:  {}, {}, {}, {}, {}, {}, {}, {} ".format(self.get_project_id(),self.get_project_name(),self.get_link(),self.get_short_description(),self.get_project_professor(),self.get_module(),self.get_project_type(),self.get_nom_of_spots())
+    def set_project_description(self, description):
+        return self._project_description == description 
+    
+    def get_project_description(self):
+        return self._project_description
+    
+
 
     @staticmethod
     def to_dict(dicti=dict()):
 
         #Convert  a Python dict() in a Project().
         project = Project()
-        project.set_id(dicti["Project_ID"])
+        project.set_project_id(dicti["Project_ID"])
         project.set_name(dicti["ProjectName"])
         project.set_link(dicti["ProjectLink"])
         project.set_room_desired(dicti["ProjectRoomDesired"])
