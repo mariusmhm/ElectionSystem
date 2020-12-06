c441335f6b618f33f35b47e8203692604fa89171
Jana Beer <49690605+Jana-Beer@users.noreply.github.com>
2020-12-06 15:20:32 +0100

    Registration added student textfields, buttons

--- frontend/src/components/layout/pages/Registration.js
+++ frontend/src/components/layout/pages/Registration.js
@@ -19,22 +19,40 @@ class Registration extends Component {
         super(props);
     
         // Init the state
-        this.state = {
-          student: false,
+        this.state = { 
+            show:false,
+            role:''
         };
+
     }
+
+    handleRadioChange = e => {
+        const  value = e.target.value;
+        console.log(value);
+        this.setState({
+            role: value,
+          });
+
+        if(value==='student'){
+            this.setState({
+                show: true
+              });
+    
+        }else{
+            this.setState({
+                show: false
+              });
+        }
+    };
         
 
     render(){
-        if(this.state.student){
-            return(
-                <Typography>Student</Typography>
-            )
-        }
+        const { classes } = this.props; 
         return (
-            <Grid container spacing={2} direction="column" justify="center" alignItems="center">
+            <Grid container spacing={2} direction="column" justify="center" alignItems="center" className={classes.grid}>
+            
                 <Grid item>
-                    <Typography>Registration</Typography> 
+                    <Typography className={classes.headline}>REGISTRATION</Typography> 
                 </Grid>
                 <Grid item>
                     <TextField fullWidth variant="outlined" label="Firstname"/>
@@ -44,16 +62,40 @@ class Registration extends Component {
                 </Grid>
                 <Grid item>
                     <FormControl>
-                        <RadioGroup row={true} onChange={handleRadioChange}>
-                            <FormControlLabel value="student" control={<Radio />} label="Student" />
-                            <FormControlLabel value="professor" control={<Radio />} label="Professor" />
-                            <FormControlLabel value="admin" control={<Radio />} label="Admin" />
+                        <RadioGroup row={true} onChange={this.handleRadioChange} value={this.state.value}>
+                            <FormControlLabel value="student" color="primary" control={<Radio />} label="Student" />
+                            <FormControlLabel value="professor" color="primary" control={<Radio />} label="Professor" />
+                            <FormControlLabel value="admin" color="primary" control={<Radio />} label="Admin" />
                         </RadioGroup>
                     </FormControl>
+                </Grid>
+            
+                {
+                    this.state.show?
+                    <Grid container spacing={2} direction="column" justify="center" alignItems="center">
+                    <Grid item >
+                        <TextField fullWidth variant="outlined" label="Matrikelnumber"/>
+                    </Grid>
+                    <Grid item>
+                        <TextField fullWidth variant="outlined" label="Course of studies"/>
+                    </Grid>
+                    </Grid>
+                    :null
+
+                }
+                
+                <Grid container row={true} justify="center" alignItems="center" spacing={2} className={classes.button}> 
+                    <Grid item>
+                        <Button variant="outlined" color="primary">Cancel</Button>
+                    </Grid>
+                    <Grid item>
+                    <Button variant="contained" color="primary">Register</Button>
+                    </Grid>
                 </Grid>
 
 
             </Grid>
+          
 
                 
             
@@ -65,14 +107,19 @@ class Registration extends Component {
 const styles = theme => ({
     grid:{
         width: '100%',
-        margin: '0px'
+        display: 'flex',
+        marginTop: theme.spacing(6)    
+    },
+    headline:{
+        color: theme.palette.darkGray,
+        fontFamily: 'Arial',
+        fontStyle: 'bold',
+        fontSize: 25
+
+    },
+    button:{
+        marginTop: theme.spacing(2)
     }
 });
-
-const handleRadioChange = (event) => {
-    this.setState({
-        student: true,
-      });
-};
 
 export default withStyles(styles)(Registration);
