1371e728956a276acab9c49247b9a1fdfd54f2b6
SarandaGojani <73344482+SarandaGojani@users.noreply.github.com>
2020-12-12 13:44:15 +0100

    created Admin Home

---
+++ frontend/src/components/layout/pages/AfterSemesterBegin/HomeAfterBegin.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import {Typography, Container} from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import HeaderAdmin from '../../HeaderAdmin';
+import ArchiveProject from './ArchiveProject';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+
+
+
+
+class HomeAfterBegin extends Component {
+
+
+evaluateNewProjektButton = (event) => {
+//soon
+}
+
+  render() {
+
+
+        return (
+
+            <div>
+
+                 <CssBaseline />
+                 <HeaderAdmin/>
+                  <Container maxWidth="sm">
+                      <ApprovedProjects/ >
+                      <Divider/>
+                      <br/>
+                      <br/>
+                      <KeyCompetence/ >
+                      <Divider/>
+                      <div>
+                      <RejectedProjects/>
+                      </div>
+                      <div>
+                      <ArchivedProjects/>
+                      </div>
+				  </Container>
+		    </div>
+		);
+	}
+}
+export default HomeAfterBegin;
