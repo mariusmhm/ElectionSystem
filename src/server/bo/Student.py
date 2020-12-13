1c8d4fafc7adab4689706400b331484369ab20c5
Marius Münstermann <73521657+muenstermannmarius@users.noreply.github.com>
2020-12-13 17:39:48 +0100

    complete studentbo

--- src/server/bo/Student.py
+++ src/server/bo/Student.py
@@ -1,70 +1,51 @@
-from server.Role import Role
 from server.bo.User import User
-from server.bo.NamedBusinessObject import NamedBusinessObject
 
 
 class Student(User):
-    """Realization of an student class."""
+    
+    #Realization of a student class
     def __init__(self):
         super().__init__()
-        self.__student_lastname = NamedBusinessObject.get_name()
-        self.__student_firstname=""
-        self.__matrikel_nr = 0
-        self.__study = ""
-
-    def set_lastname(self, student_lastname):
-        """Set the last name of a Student"""
-        self.__student_lastname = student_lastname
-
-    def get_lastname(self):
-        """Get the last name of a Student"""
-        return self.__student_lastname
-
-    def set_firstname(self, student_firstname):
-        """Set the first name of a Student"""
-        self.__student_lastname = student_firstname
-
-    def get_firstname(self):
-        """Get the first name of a Student"""
-        return self.__student_firstname
+        self._matrikel_nr = ""
+        self._study = ""
 
 
     def set_matrikel_nr(self, matrikel_nr):
-        """Set the matricle number of a Student"""
-        self.__matrikel_nr = matrikel_nr
+        #Set the matrikel number of a Student
+        self._matrikel_nr = matrikel_nr
+
 
     def get_matrikel_nr(self):
-        """Get the matricle number of a Student"""
-        return self.__matrikel_nr
+        #Get the matrikel number of a Student
+        return self._matrikel_nr
 
-    def set_study(self, student_study):
-        """Set the study of a Student"""
-        self.__study = student_study
+
+    def set_study(self, study):
+        #Set the study of a Student
+        self._study = study
+
 
     def get_study(self):
-        """Get the study of a Student"""
-        return self.__study
+        #Get the study of a Student
+        return self._study
 
-    def __str__(self):
-        """Create a correct textual representation of the guideline instance.
-        This consists of the ID of the super class, the first and last names
-        of the student, matricle number, creation date, role and study"""
-        return "Student: {}, {}, {}, {}, {}, {}, {}".format(self.get_id(), self.get_creation_date(), self.get_lastname(),
-                                                            self.get_firstname(),
-                                                            self.get_email(),
-                                                            self.get_role(), self.get_matrikel_nr(), self.get_study())
+
+    """ def __str__(self):
+        return "Student: {}, {}, {}, {}, {}, {}, {}, {}".format(self.get_id(), self.get_date(), self.get_name(), self._firstname, self._mail, self._role, self._matrikel_nr, self._study)
+ """
 
     @staticmethod
     def to_dict(dicti=dict()):
 
         """Transform a Python dict() into a Student()."""
         student = Student()
-        student.set_id(dicti["student_id"])
-        student.set_creation_date(dicti["creation_date"])
-        student.set_lastname(dicti["student_lastname"])
-        student.set_firstname(dicti["student_firstname"])
-        student.set_email(dicti["student_mail"])
-        student.set_role(dicti["student_role"])
+        student.set_id(dicti["id"])
+        student.set_date(dicti["creation_date"])
+        student.set_name(dicti["name"])
+        student.set_google_user_id(dicti["google_user_id"])
+        student.set_firstname(dicti["firstname"])
+        student.set_mail(dicti["mail"])
+        student.set_role(dicti["role"])
         student.set_matrikel_nr(dicti["matrikel_nr"])
-        student.set_study(dicti["student_study"])
+        student.set_study(dicti["study"])
         return student
