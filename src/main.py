fdcb38dfdb17695bdcbd825248cd698295795c81
SarandaGojani <73344482+SarandaGojani@users.noreply.github.com>
2020-12-13 21:25:56 +0100

    Updatet Semester functions

--- src/main.py
+++ src/main.py
@@ -42,6 +42,11 @@ student = api.inherit('Student', user, {
     'study': fields.String(attribute='_study', description='Students Study')
 })
 
+semester= api.inherit('Semester', bo, {
+    'winter_semester':fields.Boolean(attribute='_winter_semester', description='Winter Semester is true or false'),
+    'submit_projects_end_date':fields.Date(attribute='_submit_projects_end_date', description='End datum'),
+    'grading_end_date':fields.Date(attribute='_grading_end_date', description='End date of grading'),
+})
 
 
 # --- STUDENT SPECIFIC OPERATIONS ---
@@ -227,6 +232,86 @@ class UserRoleOperations(Resource):
         adm = ElectionSystemAdministration()
         users = adm.get_user_by_role(role)
         return users
+
+
+"""Semester specific functions"""
+@electionSystem.route('/semester')
+@electionSystem.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+class SemesterListOperations(Resource):
+    @electionSystem.marshal_list_with(semester)
+    def get(self):
+        """Auslesen aller Customer-Objekte.
+        Sollten keine Customer-Objekte verfügbar sein, so wird eine leere Sequenz zurückgegeben."""
+        adm = ElectionSystemAdministration()
+        semester = adm.get_all_semester()
+        return semester
+
+    @electionSystem.marshal_with(semester, code=200)
+    @electionSystem.expect(semester)  # Wir erwarten ein Customer-Objekt von Client-Seite.
+    def post(self):
+        """Anlegen eines neuen Customer-Objekts.
+        **ACHTUNG:** Wir fassen die vom Client gesendeten Daten als Vorschlag auf.
+        So ist zum Beispiel die Vergabe der ID nicht Aufgabe des Clients.
+        Selbst wenn der Client eine ID in dem Proposal vergeben sollte, so
+        liegt es an der BankAdministration (Businesslogik), eine korrekte ID
+        zu vergeben. *Das korrigierte Objekt wird schließlich zurückgegeben.*
+        """
+        adm = ElectionSystemAdministration()
+
+        proposal = Semester.to_dict(api.payload)
+
+        if proposal is not None:
+            s = adm.create_semester(proposal.get_wintersemester(),
+                                    proposal.get_submit_projects_end_date(),
+                                    proposal.get_grading_end_date())
+            return s, 200
+
+        else:
+            # Wenn irgendetwas schiefgeht, dann geben wir nichts zurück und werfen einen Server-Fehler.
+            return '', 500
+
+
+@electionSystem.route('/semester/<int:id>')
+@electionSystem.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@electionSystem.param('id', 'Die ID des Semester-Objekts')
+class SemesterOperations(Resource):
+    @electionSystem.marshal_with(semester)
+    def get(self, id):
+        """Auslesen eines bestimmten Semester-Objekts.
+        Das auszulesende Objekt wird durch die ```id``` in dem URI bestimmt.
+        """
+        adm = ElectionSystemAdministration()
+        pt = adm.get_semester_by_id(id)
+        return pt
+
+    def delete(self, id):
+        """Löschen eines bestimmten Semester-Objekts.
+        Das zu löschende Objekt wird durch die ```id``` in dem URI bestimmt.
+        """
+        adm = ElectionSystemAdministration()
+        semester = adm.get_semester_by_id(id)
+        adm.delete_semester(semester)
+        return '', 200
+
+    @electionSystem.marshal_with(semester)
+    def put(self, id):
+        """Update eines bestimmten semester-Objekts.
+        **ACHTUNG:** Relevante id ist die id, die mittels URI bereitgestellt und somit als Methodenparameter
+        verwendet wird. Dieser Parameter überschreibt das ID-Attribut des im Payload der Anfrage übermittelten
+        Customer-Objekts.
+        """
+        adm = ElectionSystemAdministration()
+        s = Semester.to_dict(api.payload)
+
+        if s is not None:
+            """Hierdurch wird die id des zu überschreibenden (vgl. Update) Account-Objekts gesetzt.
+            Siehe Hinweise oben.
+            """
+            s.set_id(id)
+            adm.save_semester(s)
+            return '', 200
+        else:
+            return '', 500
 
 
 if __name__ == '__main__':
