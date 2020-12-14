4abb2d9571f97be541fbd1eb1c3a56d18bca0f03
Amna-Mia Mujezinovic <73587707+Amna-MiaMujezinovic@users.noreply.github.com>
2020-12-14 18:36:22 +0100

    updated with projecttype methods

--- src/server/ElectionSystemAdministration.py
+++ src/server/ElectionSystemAdministration.py
@@ -9,6 +9,8 @@ from server.db.UserMapper import UserMapper
 from server.db.SemesterMapper import SemesterMapper
 from server.db.ParticipationMapper import ParticipationMapper
 from server.db.GradingMapper import GradingMapper
+from server.bo.Projecttype import Projecttype
+from server.db.ProjecttypeMapper import ProjecttypeMapper
 
 
 class ElectionSystemAdministration (object):
@@ -256,3 +258,36 @@ class ElectionSystemAdministration (object):
     def get_by_grading_id(self, id):
         with GradingMapper() as mapper:
             return mapper.find_by_id(id)
+      
+    #------------Projecttype-----------
+    
+   def get_all_projecttypes (self):
+        with ProjecttypeMapper() as mapper:
+            return mapper.find_all()
+
+    def get_projecttype_by_id (self, p_id):
+        with ProjecttypeMapper() as mapper:
+            return mapper.find_by_id(p_id)
+
+    def get_projecttype_by_name(self, name):
+        with ProjecttypeMapper() as mapper:
+            return mapper.find_by_name(name)
+
+    def update_projecttype(self, pt):
+        with ProjecttypeMapper() as mapper:
+            return mapper.update(pt)
+
+    def delete_projecttype(self, pt):
+        with ProjecttypeMapper() as mapper:
+            return mapper.delete(pt)
+
+    def create_projecttype(self,name, sws, ect):
+        projecttype = Projecttype()
+        projecttype.set_name(name)
+        projecttype.set_ect(ect)
+        projecttype.set_sws(sws)
+        projecttype.set_id(1)
+        projecttype.set_creation_date(1)
+
+        with ProjecttypeMapper() as mapper:
+            return mapper.insert(projecttype)
