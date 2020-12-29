02f1746b45e3e309225265cc61d891176ed7b79a
Jana-Beer <49690605+Jana-Beer@users.noreply.github.com>
2020-12-29 14:40:49 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,141 @@
 import React, { Component } from 'react';
-/**import Header from './components/layout/Header';**/
-import Grid from '@material-ui/core/Grid';
-/**import firebase from 'firebase/app';**/
-/**import 'firebase/auth';**/
-/**import SignIn from './components/pages/Signin';**/
-/**import LoadingProgress from './components/dialogs/LoadingProgress';**/
-/**import firebaseConfig from './firebaseconfig';**/
-/**import ContextErrorMessage from './components/dialogs/ContextErrorMessage';**/
-/**import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';**/
-
-import { Button, Icon, Container, TextField, Typography, withStyles } from '@material-ui/core';
+import Header from './components/layout/Header';
+//import Grid from '@material-ui/core/Grid';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Signin from './components/layout/pages/Signin';
+//import LoadingProgress from './components/dialogs/LoadingProgress';
+import firebaseConfig from './firebaseconfig';
+import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Container} from '@material-ui/core';
+import Registration from './components/layout/pages/Registration'
 import theme from './theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+//import { Button, Icon, Container, TextField, Typography, withStyles } from '@material-ui/core';
 import CreateProject from './components/dialogs/CreateProject'
 import ProjectContent from './components/layout/pages/ProjectContent'
-import { ThemeProvider } from '@material-ui/core/styles';
 import Semester from './components/dialogs/Semester'
 
 
 class App extends Component {
 
+    //Constructor of the ElectionSystem app, which initializes firebase
+    constructor(props) {
+	super(props);
+	//initializing a state
+	this.state = {
+		currentUser: null,
+		appError: null,
+		authError: null,
+		authLoading: false
+	};
+   }
 
-handleSignInButtonClicked = () => {
-this.props.onSignIn();
-}
-    constructor (props){
-        super(props);
-        this.state={
-        group:"Gruppe 5"
+   //Create an error boundry for this app so we can recieve all errors from below the component tree.
+   //Update state so the next render will show the fallback UI.
+   static getDerivedStateFromError(error) {
+	return { appError: error };
+   }
+
+   //Handles firebase users logged in state changes
+   handleAuthStateChange = user => {
+	if (user) {
+		this.setState({
+			authLoading: true
+		});
+		// The user is signed in
+		user.getIdToken().then(token => {
+			// Add the token to the browser's cookies. The server will then be
+			// able to verify the token against the API.
+			document.cookie = `token=${token};path=/`;
+			// Set the user not before the token arrived
+			this.setState({
+				currentUser: user,
+				authError: null,
+				authLoading: false
+			});
+			}).catch(e => {
+				this.setState({
+					authError: e,
+					authLoading: false
+				});
+			});
+		} else {
+			// User has logged out, so clear the id token
+			document.cookie = 'token=;path=/';
+
+			// Set the logged out user to null
+			this.setState({
+				currentUser: null,
+				authLoading: false
+			});
+		}
+	}
+
+    //Handles the sign in request of the SignIn component uses the firebase.auth() component to sign in.
+    handleSignIn = () => {
+		this.setState({
+			authLoading: true
+		});
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	}
+
+   //console.log("Checking if '" + name + "' is stored in database with google id '" + googleId + "'")
+   checkIfUserInDatabase(name, email, googleId) {
+	//soon
+   }
+
+   //Lifecycle method, which is called when the component gets inserted into the browsers DOM.
+   //Initializes the firebase SDK.
+   componentDidMount() {
+		firebase.initializeApp(firebaseConfig);
+		firebase.auth().languageCode = 'en';
+		firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+   }
 
 
-        }
-    }
+ /** Renders the whole app */
+  render() {
+    const { currentUser, appError, authError, authLoading } = this.state;
 
-    render() {
         return (
-            <ThemeProvider theme={theme}>
-                <div>
-                    <Semester /> {/* INSERT HERE WHAT YOU WANT TO TEST */}
-                </div>
-            </ThemeProvider>
-        )
-  }
 
+            <div>
+                <ThemeProvider theme={theme}>
+			<CssBaseline />
+				<Router basename={process.env.PUBLIC_URL}>
+                        	<Container maxWidth='md'>
+                                	<Header user={currentUser} />
+                               		{
+					        // Is a user signed in?
+						currentUser ?
+							<>
+							<Redirect to='/Registration'/>
+							<Route path='/Registration'><Registration /></Route>
+                                       			</>
+							:
+							// else show the sign in page
+							<>
+							<Redirect to='/Signin'/>
+							<Signin handleSignIn={this.handleSignIn} />
+							</>
+					}
+					<LoadingProgress show={authLoading} />
+					<ContextErrorMessage error={authError} contextErrorMsg={'Etwas ist schief gelaufen während dem Prozess'} onReload={this.handleSignIn} />
+					<ContextErrorMessage error={appError} contextErrorMsg={'Etwas ist schief geleaufen in der App. Bitte lade die Seite neu.'} />
+
+                        	</Container>
+                        	</Router>
+			</ThemeProvider>
+		    </div>
+		);
+	}
 }
-
-
-
-/**PropTypes
-SignIn.propTypes = {
-	classes: PropTypes.object.isRequired,
-	onSignIn: PropTypes.func.isRequired,}**/
-
 export default App;
+
 
 
