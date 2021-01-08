c67df60ba9e13f78f0497c0e165dc2228241f1a6
Marie-We <mw220@hdm-stuttgart.de>
2021-01-08 15:43:20 +0100

    Create AddStudent pop-up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import Teilnehmerliste from './components/layout/pages/Teilnehmerliste'
 import ArchiveProject from'./components/layout/pages/ProjectGenehmigen/ArchiveProject'
 import ListEntryDiesesSemester from'./components/layout/pages/ProjectGenehmigen/ListEntryDiesesSemester'
 import HomeScreenAdminBeginn from'./components/layout/pages/ProjectGenehmigen/HomeScreenAdminBeginn'
-
+import AddStudent from'./components/dialogs/AddStudent'
+import CreateProject from'./components/dialogs/CreateProject'
 
 class App extends Component {
 
@@ -15,7 +16,7 @@ class App extends Component {
         return (
 
             <div>
-                <Teilnehmerliste />
+                <CreateProject />
 		    </div>
 		);
 	}
