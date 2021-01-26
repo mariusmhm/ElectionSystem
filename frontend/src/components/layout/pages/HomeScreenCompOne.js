79a5782d48d5867056458ce365273fd65a75f72c
Adrian Awad <adrian.awad95@gmail.com>
2021-01-26 19:25:02 +0100

    Generating Arrays with selected or unselected

--- frontend/src/components/layout/pages/HomeScreenCompOne.js
+++ frontend/src/components/layout/pages/HomeScreenCompOne.js
@@ -3,33 +3,14 @@ import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+
 import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-import { AppBar, Tabs, Tab, withStyles, Collapse, Card, Paper, Box, Divider, ButtonBase } from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import TreeView from '@material-ui/lab/TreeView';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import TreeItem from '@material-ui/lab/TreeItem';
+import { AppBar, Tabs, Tab, withStyles, Collapse, Card, Paper, Box, Divider, ButtonBase, TableSortLabel } from '@material-ui/core';
+
 import { makeStyles } from '@material-ui/core/styles';
 import { ElectionSystemAPI, ProjectBO, ParticipationBO, ProjecttypeBO } from '../../../api';
-import { id, ja } from 'date-fns/locale';
-import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+
 import TableEntry from '../../assets/TableEntry';
-import SelectedProjectEntry from '../../assets/SelectedProjectEntry';
-
-
 
 
 class HomeScreenCompTwo extends Component {
@@ -37,62 +18,30 @@ class HomeScreenCompTwo extends Component {
         super(props)
         this.state = {
             currentUser: 2,
-            tableData: [],
+            number: 4,
+            participationID: 1,
+            activeIndex: null,
+
             projects: [],
             projecttypes: [],
             participations: [],
             selectedProjects: [],
+            unselectedProjects: [],
+
             participationLoaded: false,
             projectLoaded: false,
+            projecttypeLoaded: false,
+            loaded: true,
+
             error: null,
             priority: '',
             updatingError: null,
             deletingError: null,
-            loaded: null,
-            activeIndex: null,
-            number: 4,
-            participationID: 1,
-
-
 
         };
+
         this.baseState = this.state;
         
-        
-        
-    }
-
-    getParticipationsForStudent = () => {
-        ElectionSystemAPI.getAPI().getParticipationsForStudent(this.state.currentUser)
-        .then(ParticipationBO =>
-            this.setState({
-                participations: ParticipationBO,
-                participationLoaded: true,
-                error: null
-            })).catch(e => 
-                this.setState({
-                    participationLoaded: [],
-                    error: e
-                }))
-        console.log('Participations loaded from Backend');
-        
-        
-    }
-    
-
-    getAllProjects = () => {
-        ElectionSystemAPI.getAPI().getAllProjects()
-            .then(projectBO =>
-                this.setState({
-                    projects: projectBO,
-                    projectLoaded: true,
-                    error: null
-                })).catch(e =>
-                    this.setState({
-                        projects: [],
-                        error: e
-                    }))
-        console.log('Project loaded from Backend');
     }
 
     getAllProjecttypes = () => {
@@ -100,7 +49,7 @@ class HomeScreenCompTwo extends Component {
             .then(ProjecttypeBO =>
                 this.setState({
                     projecttypes: ProjecttypeBO,
-                    loaded: true,
+                    projecttypeLoaded: true,
                     error: null
                 })).catch(e =>
                     this.setState({
@@ -111,138 +60,178 @@ class HomeScreenCompTwo extends Component {
     }
 
 
-    
+    getParticipationsForStudent = () => {
+        ElectionSystemAPI.getAPI().getParticipationsForStudent(this.state.currentUser)
+        .then(ParticipationBO =>
+            this.setState({
+                participations: ParticipationBO,
+                participationLoaded: true,
+                error: null
+            }))
+            
+        .then(this.getAllProjects)
+        
+        
+        .catch(e => 
+
+                this.setState({
+                    participationLoaded: [],
+                    error: e
+
+                }))
+        
+    }
+
+    getAllProjects = () => {
+        ElectionSystemAPI.getAPI().getAllProjects()
+            .then(projectBO =>
+
+                
+                this.setState({
+                    projects: projectBO,
+                    projectLoaded: true,
+                    error: null
+                }))
+
+            .then(this.sortProjects)
+                      
+
+            .catch(e =>
+                this.setState({
+                    projects: [],
+                    error: e
+                }))
+        
+    }
+
+    sortProjects = () =>{
+        
+        this.state.projects.forEach(project=> {
+
+            if(this.state.participations.filter(part => part.project_id === project.id).length > 0 ){
+                
+                this.setState(prev => ({
+                    selectedProjects: [...prev.selectedProjects, project]
+                }))
+                
+            }
+
+            else{
+                
+                this.setState(prev => ({
+                    unselectedProjects: [...prev.unselectedProjects, project]
+                }))               
+            }
+
+        }).then(console.log('Ready'))
+
+
+    }
+
+
 
     componentDidMount() {
-        this.getAllProjects();
+        
         this.getParticipationsForStudent(); 
-        this.getAllProjecttypes();   
+        this.getAllProjecttypes(); 
+       
+        
     }
+
 
 
 
 
     render() {
-        const { projects, selectedProjects, participationLoaded, projectLoaded, participations,projecttypes } = this.state;
+        const { projects, participations,projecttypes,selectedProjects, unselectedProjects } = this.state;
 
-        console.log('Projekte: ' + projects.length)
-        console.log('Pr')
-        console.log('Selected: ' + selectedProjects.length)
-        console.log(projects.filter(project => project.id === 1))
+
+        console.log('All Projects: ' + projects)
+        console.log('All Participations: ' + participations)
+        console.log('All Projecttypes: ' + projecttypes)
+        console.log('Selected: '+ selectedProjects)
+        console.log('Not Selected: '+ unselectedProjects)
 
 
         return (
             <div>
-                <Grid container>
-                    <Grid container>
-                        {projects.length > 0  ? 
-                            <>
-                            {
-                                projecttypes.map(pt =>
-                                    <>
-                                    <Grid item xs={12}><Typography>{pt.getName()}</Typography></Grid>
-                                    {
-                                        projects.map(p =>
-                                            participations.filter(ptp => ptp.project_id === p.getID()).length > 0 ? 
-                                            pt.getID() === p.getProjecttype() ?
-                                            <>
-                                            {console.log('Gefunden: ' + participations.find(ptpID => ptpID.project_id === p.getID()).id)}
-                                        
-                                            <Grid container xs={12}>
-                                                
-                                            <TableEntry
-                                                key = {p.getID()}
-                                                id = {p.getID()}
-                                                name = {p.getName()}
-                                                prof = {p.getProfessor()}
-                                                dsc = {p.getShortDescription()}
-                                                ects = {pt.getEcts()}
-                                                sws = {pt.getSws()}
-                                                participationID = {participations.find(ptpID => ptpID.project_id === p.getID()).id}
-                                                
-                                            />
-                                            </Grid>
-                                           
-                                            </>
-                                            :
-                                                null
-                                            :
-                                                null
-                                                
-                                            
-                                            
-                                        )
-                                    }
-                                    
-                                    </>
-                                    
-                                    
-                                )
+                <Grid container >
+                    {selectedProjects.length > 0 ? 
+                    
+                            projecttypes.map(pt => 
+
+                                selectedProjects.filter(p => p.projecttype_id ===pt.id).length > 0 ?
                                 
-                            } 
-                            </>
-                            : 
-                            <>
-                            <b>Loading</b>
-                            </>
-                        }
-                    </Grid>
-                <Grid item xs={12}>
-                    <Divider/>
-                </Grid>
+                                    <Grid item xs={12}>
+                                        <Typography>{pt.getName()}</Typography>
+                                        {
+                                            selectedProjects.map(project =>
+                                                
+                                                project.projecttype_id === pt.id ?
+
+                                                <>
+                                                <Typography>{project.getID()}</Typography>
+                                                </>
+
+                                                :null
+
+                                            )
+                                        }
+                                    </Grid>
+                                
+                                :null
+                                    
+                            )     
+                        
+                        :null
                 
-                <Grid container>
-                    {projects.length > 0  ? 
-                        <>
-                        {
-                            projecttypes.map(pt =>
-                                <>
+                    }
+
+                <Grid item xs={12}>
+                <Divider/>
+                <Typography>Hier kommen nicht gewählte</Typography>
+                <Divider/>
+                </Grid>
+
+                {unselectedProjects.length > 0 ? 
+                    
+                    projecttypes.map(pt => 
+
+                        unselectedProjects.filter(p => p.projecttype_id ===pt.id).length > 0 ?
+                        
+                            <Grid item xs={12}>
                                 <Typography>{pt.getName()}</Typography>
                                 {
-                                    projects.map(p =>
-                                        participations.filter(ptp => ptp.project_id === p.getID()).length > 0 ? 
-                                            null
-                                        :
-                                            pt.getID() === p.getProjecttype() ?
-                                            <Grid container xs={12}>
-                                            <TableEntry
-                                                id = {p.getID()}
-                                                name = {p.getName()}
-                                                prof = {p.getProfessor()}
-                                                dsc = {p.getShortDescription()}
-                                                ects = {pt.getEcts()}
-                                                sws = {pt.getSws()}  
-                                            />
-                                            </Grid>
-                                            :
-                                            null
-                                            
+                                    unselectedProjects.map(project =>
                                         
+                                        project.projecttype_id === pt.id ?
+
+                                        <>
+                                        <Typography>{project.getID()}</Typography>
+                                        </>
+
+                                        :null
+
                                     )
                                 }
-                                </>
-                                
-                            )
+                            </Grid>
+                        
+                        :null
                             
-                        } 
-                        </>
-                        : 
-                        <>
-                        <b>Loading</b>
-                        </>
-                    }
-                </Grid>
+                    )     
                 
-                
-                    <Grid item xs={12}>
-                        <Button onClick={() => window.location.reload(false)}>Click to reload</Button>
-                    </Grid>
-
+                :null
+        
+            }
                 </Grid>
             </div>
         );
     }
 }
+
+
+
+
 const styles = theme => ({
     grid: {
         width: '100%',
