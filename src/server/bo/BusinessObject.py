63f9a89434c2f748a562af6d09d258edb7229e02
Marie-We <mw220@hdm-stuttgart.de>
2021-01-29 15:00:28 +0100

    Comment

--- src/server/bo/BusinessObject.py
+++ src/server/bo/BusinessObject.py
@@ -8,13 +8,17 @@ class BusinessObject(ABC):
         self._creation_date = ''
 
     def get_id(self):
+        # get id of an business Object
         return self._id
 
     def set_id(self, val):
+        # set id of an business Object
         self._id = val
 
     def get_date(self):
+        # get date of an business Object
         return self._creation_date
 
     def set_date(self, creation_date):
+        # set the creation date of an business Object
         self._creation_date = creation_date
